18f5435b91c4fd94de313afcb277082eee1e5470
Sushant Bansal <sushantbansal2@gmail.com>
2020-08-16 01:47:58 -0500

    Added homepage, navbar, router setup and footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Metronome from "./Metronome";
+import About from "./About";
+
+import "./App.css";
+import github from "./static/images/github.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <Router>
+      <div className="row">
+        <Link to="/" title="Go to Metronome">
+          <h1 className="title">Metronome</h1>
+        </Link>
+        <Link to="/about" title="Go to About Page">
+          <button className="button">About</button>
+        </Link>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/anti-mony/metronome"
           target="_blank"
           rel="noopener noreferrer"
+          title="Link to the Github Repository"
         >
-          Learn React
+          <img
+            src={github}
+            className="button"
+            alt="Github Logo (Octocat Shape)"
+          />
         </a>
-      </header>
-    </div>
+      </div>
+      <div className="content">
+        <Switch>
+          <Route exact path="/" component={Metronome} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+      <div className="row footer"> ALWAYS BE ON TIME </div>
+    </Router>
   );
 }
 
